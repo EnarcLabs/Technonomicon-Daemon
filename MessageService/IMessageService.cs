f2ac42d72baf33cc46bdbfbb22a4512f4ea40cd4
Olivia Trewin <zancoproductions@gmail.com>
2018-05-04 23:42:00 -0500

    Dummy email confirmation
    Added dummy email confirmation ability
    Still not sure if this is the way to go, the email confirmation tokens are really long. So should the api submit this or the user themselves? Idk.

---
+++ MessageService/IMessageService.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EnarcLabs.Technonomicon.Daemon.MessageService
+{
+    public interface IMessageService
+    {
+        /// <summary>
+        /// Sends an email.
+        /// </summary>
+        /// <param name="mail">The message to send.</param>
+        /// <returns>A task for the send operation.</returns>
+        Task<SendResults> Send(MailMessage mail);
+    }
+
+    
+    public enum SendResults
+    {
+        Success = 0,
+        Error = 1,
+    }
+}
